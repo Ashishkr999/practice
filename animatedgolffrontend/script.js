d374614cde3db337a96ec71da28f2d60830c1d18
ashishkr <singhashishgg2002@gmail.com>
2024-09-17 12:33:55 +0530

    commited it first time

---
+++ animatedgolffrontend/script.js
@@ -0,0 +1,44 @@
+var crsr = document.querySelector("#cursor");
+document.addEventListener("mousemove",(dets)=>{
+     crsr.style.left = dets.x +"px"
+     crsr.style.top = dets.y+"px"
+
+})
+var crsrblur = document.querySelector("#cursorblur");
+document.addEventListener("mousemove",(dets)=>{
+     crsrblur.style.left = dets.x-250 +"px"
+     crsrblur.style.top = dets.y-250+"px"
+
+})
+
+
+
+
+gsap.to("#nav",{
+     backgroundColor:"#000",
+     height:"100px",
+     duration:0.5,
+     scrollTrigger:{
+          trigger:"#nav",
+          scroller:"body",
+          markers:true,
+          start:"top -10%",
+          end:"top -12%",
+          scrub:1,
+     }
+})
+gsap.to("#main",{
+     backgroundColor:"black",
+     scrollTrigger:{
+          trigger:"#main",
+          scroller:"body",
+          markers:true,
+          start:"top -25%",
+          end:"top -70%",
+          scrub:2,
+          
+     }
+})
+
+var video = document.getElementById("myVideo");
+     video.playbackRate = 2;
